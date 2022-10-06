c773903962578d539bd55c12fb3548f73f367f9f
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2022-10-06 18:45:44 +1300

    dev22_40a - Setup Items and Blocks, Add Chainmail Armour Code.

--- src/main/java/com/mclegoman/beekeeper/registry/items.java
+++ src/main/java/com/mclegoman/beekeeper/registry/items.java
@@ -7,6 +7,7 @@ import com.mclegoman.beekeeper.main;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterials;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,40 +16,40 @@ import static com.mclegoman.beekeeper.main.MOD_ID;
 
 public class items {
     public static final Item GOLDEN_CHAINMAIL_HELMET = registerItem("golden_chainmail_helmet",
-            new ArmorItem(armorMaterials.GOLD_CHAIN, EquipmentSlot.HEAD,
+            new ArmorItem(ArmorMaterials.GOLD, EquipmentSlot.HEAD,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item GOLDEN_CHAINMAIL_CHESTPLATE = registerItem("golden_chainmail_chainmail",
-            new ArmorItem(armorMaterials.GOLD_CHAIN, EquipmentSlot.CHEST,
+            new ArmorItem(ArmorMaterials.GOLD, EquipmentSlot.CHEST,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item GOLDEN_CHAINMAIL_LEGGINGS = registerItem("golden_chainmail_leggings",
-            new ArmorItem(armorMaterials.GOLD_CHAIN, EquipmentSlot.LEGS,
+            new ArmorItem(ArmorMaterials.GOLD, EquipmentSlot.LEGS,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item GOLDEN_CHAINMAIL_BOOTS = registerItem("golden_chainmail_boots",
-            new ArmorItem(armorMaterials.GOLD_CHAIN, EquipmentSlot.FEET,
+            new ArmorItem(ArmorMaterials.GOLD, EquipmentSlot.FEET,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item DIAMOND_CHAINMAIL_HELMET = registerItem("diamond_chainmail_helmet",
-            new ArmorItem(armorMaterials.DIAMOND_CHAIN, EquipmentSlot.HEAD,
+            new ArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.HEAD,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item DIAMOND_CHAINMAIL_CHESTPLATE = registerItem("diamond_chainmail_chainmail",
-            new ArmorItem(armorMaterials.DIAMOND_CHAIN, EquipmentSlot.CHEST,
+            new ArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.CHEST,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item DIAMOND_CHAINMAIL_LEGGINGS = registerItem("diamond_chainmail_leggings",
-            new ArmorItem(armorMaterials.DIAMOND_CHAIN, EquipmentSlot.LEGS,
+            new ArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.LEGS,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item DIAMOND_CHAINMAIL_BOOTS = registerItem("diamond_chainmail_boots",
-            new ArmorItem(armorMaterials.DIAMOND_CHAIN, EquipmentSlot.FEET,
+            new ArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.FEET,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item NETHERITE_CHAINMAIL_HELMET = registerItem("netherite_chainmail_helmet",
-            new ArmorItem(armorMaterials.NETHERITE_CHAIN, EquipmentSlot.HEAD,
+            new ArmorItem(ArmorMaterials.NETHERITE, EquipmentSlot.HEAD,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item NETHERITE_CHAINMAIL_CHESTPLATE = registerItem("netherite_chainmail_chainmail",
-            new ArmorItem(armorMaterials.NETHERITE_CHAIN, EquipmentSlot.CHEST,
+            new ArmorItem(ArmorMaterials.NETHERITE, EquipmentSlot.CHEST,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item NETHERITE_CHAINMAIL_LEGGINGS = registerItem("netherite_chainmail_leggings",
-            new ArmorItem(armorMaterials.NETHERITE_CHAIN, EquipmentSlot.LEGS,
+            new ArmorItem(ArmorMaterials.NETHERITE, EquipmentSlot.LEGS,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
     public static final Item NETHERITE_CHAINMAIL_BOOTS = registerItem("netherite_chainmail_boots",
-            new ArmorItem(armorMaterials.NETHERITE_CHAIN, EquipmentSlot.FEET,
+            new ArmorItem(ArmorMaterials.NETHERITE, EquipmentSlot.FEET,
                     new FabricItemSettings().group(itemGroup.BEEKEEPER)));
 
     private static Item registerItem(String name, Item item) {
