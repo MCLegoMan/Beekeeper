407cef742bf2ae9dc1c2def09ab7160cd831f0d3
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2022-10-06 15:58:11 +1300

    dev22_40a - Setting up project

--- src/main/java/com/mclegoman/beekeeper/main.java
+++ src/main/java/com/mclegoman/beekeeper/main.java
@@ -1,21 +1,23 @@
-package net.fabricmc.example;
+// Copyright (c) Daniel-Tayden Larsen 2022 //
+// License: MCLM-OSL (2022) //
+
+package com.mclegoman.beekeeper;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+public class main implements ModInitializer {
+	public static final String MOD_ID = "beekeeper";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+	}
+
+	public static Identifier identifier(String path) {
+		return new Identifier(MOD_ID, path);
 	}
 }
